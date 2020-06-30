d3e759a8e792e63cfcf0b225da539ca3606057ad
jinshuai-wang <56749767+jinshuai-wang@users.noreply.github.com>
2020-06-30 15:32:37 +0800

    Create singleHungry.cpp

---
+++ 单例模式/singleHungry.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Singleton{
+private:
+		Singleton(){
+			cout<<"Singleton constructor"<<endl;
+			count++;
+		}
+		
+		class ReleaseSingle{
+		public:
+			ReleaseSingle(){
+				cout<<"ReleaseSingle constructor"<<endl;
+			}
+			virtual ~ReleaseSingle(){
+				cout<<"ReleaseSingle destructor"<<endl;
+				//delete Singleton::instance;
+			}
+		};
+		
+		
+		Singleton &operator=(const Singleton&)=delete;//不允许赋值
+		Singleton(const Singleton&)=delete;//不允许复制
+		//static Singleton *instance;
+		
+		static int count;
+public:
+		virtual ~Singleton(){
+			cout<<"Singleton destructor"<<endl;
+		}
+		
+		//getInstance 是静态函数，否则需要对象去调用
+		static Singleton *getInstance()
+		{
+			static ReleaseSingle releaseInstance;
+			static Singleton instance;
+			return &instance;
+		}
+		void print()
+		{
+			cout<<"count = "<<count<<endl;
+		}
+};
+
+//静态变量的初始化在类外，并且不在需要static修饰
+//Singleton* Singleton::instance = new Singleton();
+int Singleton::count = 0;
+
+int main()
+{
+	Singleton *instance1 = Singleton::getInstance();
+	Singleton* instance2 = Singleton::getInstance();
+	instance1->print();
+	instance2->print();
+}
